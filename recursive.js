abf8957d5c70c67337f90a4dd04e122a87516ead
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-09-17 10:34:06 -0500

    Update ejercice

--- recursive.js
+++ recursive.js
@@ -1,10 +1,28 @@
+/* 
+Copyright (C) <2022>  <Jakepys> <j4kyjak3@protonmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /*
   ==============================================================================================================
-  Imprimir los numeros de forma invertida, por ejemplo si el numero es 5,
-  debera imprimir 5,4,3,2,1. Si el numero llega a 1, imprimir terminaste.
-  Si el numero ingresado es 0. Imprimir "Para que perder el tiempo".
-  Dato: los numeros imprimidos deben guardarse en un array de forma FIFO <Primero en entrar, primero en salir>
-  y LIFO “último en entrar, primero en salir” "Demostrarlo".
+  Dado este ejercico, debe crear un array de numeros donde solo almacenara los nunmero del 10 hacia atras,
+  creara una variable contador que debe aumentar en 1 cada vez que se agrege un elemento al array.
+  La funcion rev recive un numero, ese numero debe restarse en 1 hasta llegar a la condicion de parada de la funcion.
+  Comparar contador deber retornar true si es igual al length de el array.
+  La funcion dev debe retornar Termine.
+  LLamar  a la funcion rev y pasarle el numero pedido
   Debe usarse recursion, no estan permitidos bucles for, while, do while etc...
   ==============================================================================================================
 */
@@ -16,10 +34,10 @@ function rev(n) {}
 
 function compararContador(contador) {}
 
+
 module.exports = {
+  contador,
+  arrNumero,
   rev,
   compararContador,
-  arrNumero,
-  contador,
 };
-
